20c510554135483ed1d570dfb249b9a4f761116d
mohammad saad ali <38328235+skahmerali@users.noreply.github.com>
2024-09-09 22:55:51 +0500

    js start

---
+++ JAVASCRIPT/class 01/app.js
@@ -0,0 +1,41 @@
+// alert("HELLO CLASS 1...");
+// alert("HELLO CLASS 2...");
+// alert("HELLO CLASS 3...");
+
+
+// variables
+// Jaffar Aman
+
+// username  = "Jaffar Aman"
+// username
+
+
+
+// var username = "Jaffar Aman";
+// alert("hello")
+// alert(username)
+
+// Data Types
+// 1. String => "" , '' , group of char
+// 2. Number
+// 3. Boolean
+// 4. null
+// 5. undefined 
+
+// String
+// var a = "smit"; // String Data Type
+
+// Number Data type
+// var num1 = 101; //Number Data type
+// alert(num1)
+
+
+
+
+// var let const
+// var num1 = 101;
+
+alert("HELLO 1");
+alert("HELLO 2");
+alert("HELLO 3");
+// compiler vs interpreter vs assembler
