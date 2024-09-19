b17518aaf7650843821126e918519f15114202e5
skahmerali <sheikhahmerali75@gmail.com>
2024-09-19 22:40:36 +0500

    if statements

---
+++ JAVASCRIPT/class 03/app.js
@@ -0,0 +1,88 @@
+// console.log("Hello class...")
+
+// console.log(10 - "20a")
+
+// var fullName = "Jaffar Aman";
+// console.log("fullName" , fullName)
+
+// var userName = prompt("Enter Your User Name!");
+// console.log(userName, "userName");
+
+// var userName = prompt("Enter Your user Name" , "John Doe")
+
+// var num1 = +prompt("Enter Number 1")
+// var num2 = +prompt("Enter Number 2")
+// console.log(num1 + num2 , "Add")
+// console.log(num1 - num2 , "Sub")
+// console.log(num1 * num2 , "Mul")
+// console.log(num1 / num2 , "Div")
+
+// console.log("allow")
+// console.log("not allow")
+
+// if= > true
+// else = > false
+// if statements
+// if (condition) {
+//   // block of statements
+//   console.log("Allow");
+// } else {
+//   console.log("not allow");
+// }
+
+// =
+// ==
+// ===
+
+// var age = 20;
+// console.log(age)
+// if(age == 18){
+//     console.log("Allow")
+// }else{
+//     console.log("Not Allow")
+// }
+// console.log(age)
+
+// =
+// ==
+
+// var bool = false => 0
+// var bool = true =>  1
+
+// var userAge = +prompt("enter your age");
+// if(userAge === 25){
+//   console.log("Allow");
+// } else {
+//   console.log("Not Allow");
+// }
+
+// >
+// <
+// >=
+// <=
+
+// var userAge = 25
+
+// if(userAge >= 18){
+//     console.log("addmission allow")
+// }else{
+//     console.log("not allow")
+// }
+
+// && => And Gates => true && true
+// || => OR Gates => true || false
+
+// var age = 52;
+// if (age >= 18 || age <= 50) {
+//   console.log("ALLOW");
+// } else {
+//   console.log("Not Allow");
+// }
+
+var age = 28;
+var gender = "male";
+if ((age >= 18 && age <= 50 && gender == "male") || gender == "female") {
+  console.log("Allow");
+} else {
+  console.log("Not Allow");
+}
