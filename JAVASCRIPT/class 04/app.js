f3327de805bbf037545d46c41c8d3bfcc5fe6ee7
mabdullahjs <mabdullah2037@outlook.com>
2024-09-23 23:04:47 +0500

    if statement and array start

---
+++ JAVASCRIPT/class 04/app.js
@@ -0,0 +1,142 @@
+// if => condition true
+// else => condition false
+
+// var condition = true
+
+// if(condition == true){
+//     console.log("Hello class  1...");
+// }else{
+//     console.log("Hello class  2...");
+// }
+
+// =
+// ==
+// ===
+// >
+// <
+// >=
+// <=
+
+// && => AND GATE =>   true && true => true
+// || => OR GATES =>   false || true => true
+
+// OR GAtes
+// 1 || 1 = 1
+// 1 || 0 = 1
+// 0 || 1 = 1
+// 0 || 0 => 0
+
+// AND GAtes
+// 1 && 1 = true
+// 1 && 0 = 0
+// 0 && 1 = 0
+// 0 && 0 => 0
+
+// if()
+
+// var userAge = +prompt("Enter your age...");
+
+// if (userAge >= 16 && userAge <= 50) {
+//   console.log("Allow");
+// } else {
+//   console.log("Not Allow");
+// }
+
+// if (userAge >= 16 || userAge <= 50) {
+//   console.log("Good Morning");
+// } else {
+//   console.log("Not Allow");
+// }
+
+// if =>true
+// else => false
+// else if=> multi if
+
+// var userTime = prompt("Enter time");
+// if (userTime >= 0 && userTime <= 6) {
+//   alert("Good night!");
+// } else if (userTime >= 7 && userTime <= 12) {
+//   alert("Good Morning!");
+// } else if (userTime >= 13 && userTime <= 18) {
+//   alert("Good afternoon!");
+// } else if (userTime >= 19 && userTime <= 23) {
+//   alert("Good Evening!");
+// } else {
+//   alert("Invalid Time");
+// }
+
+// console.log("HELLO WORLD")
+
+// 0 - 500 => 0%
+// 501 - 1000 => 5%
+// 1001 - 3000 => 10%
+// 3001 - 10000 => 20%
+// 10001 - infinite => 35%
+
+// var amount  = prompt("Enter your amount...")
+// if(amount>=0 && amount<=500){
+//     alert("0% discount")
+// }else if(amount > 500 && amount<=1000){
+//     alert("5% discount")
+// }else if(amount > 1000 && amount<=3000){
+//     alert("10% discount")
+// }else if(amount > 3000 && amount<=10000){
+//     alert("20% discount")
+// }else if(amount > 10000){
+//     alert("35% discount")
+// }else{
+//     alert("Invalid amount")
+
+// }
+
+// var email = prompt("Enter email address");
+// var password = prompt("Enter password");
+
+// if (email === "admin@gmail.com" && password === "admin123") {
+//   console.log("login successfully!");
+// } else {
+//   console.log("Invalid email & password!");
+// }
+
+// if (email === "admin@gmail.com") {
+
+//     if(password === "admin123"){
+//         console.log("theeeeeeeek hai login successfully!")
+//     }else{
+//         console.log("Invalid Password")
+//     }
+
+// }else{
+//     console.log("Invalid Email!")
+// }
+
+// Arrays
+
+// var city1 = "Karachi";
+// var city2 = "lahore";
+// var city3 = "Multan"
+// var fruit1 = "Apple";
+// var fruit2 = "orange";
+// var fruit3 = "mango";
+
+// Data type
+// 1.String
+// 2.Number
+// 3.Boolean
+// 4.null
+// 5.undefined
+
+// 1.Array
+// 2.Object
+// 3.Functions
+
+// var cities = ["karachi", "lahore", "multan" , "islamabad"];
+
+// index Number start =>  0
+
+// console.log(cities[1] , cities[2])
+
+// var cities = ["lahore" , "multan"];
+
+// cities[5] = "karachi";
+// console.log(cities[4], "cities");
