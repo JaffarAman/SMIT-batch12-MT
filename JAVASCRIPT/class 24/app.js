cb6bcda023b63e10285ad3f66749d1754463f728
Shehzad Iqbal <sheikhahmerali75@gmail.com>
2024-12-02 23:10:29 +0500

    advance JS

---
+++ JAVASCRIPT/class 24/app.js
@@ -0,0 +1,112 @@
+// function loginHandler(){
+//     console.log("window" , window.location.href)
+//     var isLogin  = true
+
+//     // if(isLogin){
+//     //     // window.location.href = "./about.html"  //property
+//     //     // window.location.assign("./about.html") // method
+
+//     //     window.location.replace("./about.html")
+//     // }else{
+//     //     alert("Invalid email or password")
+//     // }
+
+
+//     //open page on new Tab
+//     window.open("./about.html" , "win1" , "width=350,height=350")
+
+
+// }
+
+// function refreshHandler(){
+//     window.location.reload()
+// }
+
+
+// variables
+
+
+// ES5 => Basic JS
+// ES6 => Advance JS
+
+// var firstName = "Jaffar"
+// console.log("firstName", firstName)
+
+
+
+// var  , let , const
+
+
+// var userName = "Jaffar"
+
+
+// document.write(userName)
+
+
+
+
+// var userName = "Aman"
+// document.write(userName)
+
+// console.log("userName", userName)
+
+
+
+// var percentage = 20 ;
+
+
+// percentage = 30
+
+// console.log(percentage)
+
+
+// let userName = "Jaffar Aman"
+// console.log(userName)
+
+// userName = "Muhammad Sufiyan"
+// console.log(userName)
+
+// let userName2
+
+// const percentage = 20
+// percentage = 40
+// console.log(percentage)
+
+// const per;
+
+// var => same name multiply variables
+// let => unique name sy variables
+// const => unique name ++ constant value
+
+// if(){
+
+// }
+// for(){}
+// function(){}
+
+// let a = 20
+// if(true){
+//      a = 10 // a is block scope variable
+// }
+// console.log(a , "a")
+
+// var = > function scope
+// let = > block scope => {  }
+// const = > block scope => {  }
+
+
+// Hoisting
+
+
+//top 1000 lines
+// var firstName;
+console.log(firstName)
+
+var firstName = "Jaffar"
+
+
+
+
+
+
+
