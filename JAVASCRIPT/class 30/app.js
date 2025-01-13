af7ddb7835d8b33f3703d04ba67b2dd2b0274fa5
JaffarAman <amanjaffar50@gmail.com>
2025-01-13 21:54:12 +0500

    advance js topics

---
+++ JAVASCRIPT/class 30/app.js
@@ -0,0 +1,125 @@
+// var myName = "Jaffar"
+// var b = myName
+// myName = "Jaffar Aman"
+// console.log(b, "b")
+
+
+// var a = {
+//     name: "Jaffar Aman"
+// }
+
+// // var b = a
+// // // b.name = "Jaffar"
+
+// // a.name = "sufiyan"
+// // b.age = 20
+
+// console.log("obj a", a)
+// console.log("obj b", b)
+
+
+
+
+// const objA = {
+//     firstName: "Jaffar",
+//     lastName: "Aman",
+// }
+
+// const objB =  { ...objA }  //deep copy
+
+// objA.firstName = "John"
+// objA.lastName = "Doe"
+
+// objB.age = 20
+
+// console.log("objA", objA)
+// console.log("objB", objB)
+
+
+
+// const arr1 = ["apple", "mango"]
+
+
+// const arr2 = [...arr1]
+
+// arr1[2] = "orange"
+// console.log(arr2, "arr2")
+
+// const obj1 = {
+//     name: "Jaffar Aman",
+//     address: {
+//         city: "karachi",
+//         country: "Pakistan"
+//     },
+//     age: 23,
+//     gender: "male"
+// }
+
+
+// // const obj2 = { ...obj1 }
+
+// let obj2 = JSON.stringify(obj1)
+// obj2 = JSON.parse(obj2)
+
+// obj1.name = "sufiyan"
+// obj1.address.city = "lahore"
+// console.log("obj2", obj2)
+
+
+
+
+// const sum = (...args) => {
+//     console.log(args)
+//     var total = 0
+//     for (var i = 0; i < args.length; i++) {
+//         total += args[i]
+//     }
+//     console.log("total", total)
+// }
+
+// sum(1, 2, 3, 4, 5, 6, 479, 9,)
+
+
+
+// const foo = () => {
+//     console.log("HELLO WORLD")
+//     foo()
+// }
+
+// foo()
+
+
+
+// let count = 1
+
+// function foo() {
+//     console.log(count)
+//     count++
+//     if (count <= 10) {
+//         foo()
+//     }
+// }
+
+// foo()
+
+
+
+
+// IIFE
+// const foo = (() => {
+//     console.log("foo IIFE")
+// })();
+
+
+// (function foo1() {
+//     console.log("foo1")
+// })()
+
+// const arr = ["karachi" , "lahore"]
+
+// const findValue = arr.find(  (value , index)=>{
+
+// }  )
+
+
+
